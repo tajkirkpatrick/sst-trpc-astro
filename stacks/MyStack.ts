bbbe2c8799f55834393c613131fafc160cb6457f
Taj Kirkpatrick <taj.kirkpatrick@gmail.com>
2023-08-27 23:57:26 -0500

    init commit

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -2,6 +2,7 @@ import { StackContext, Api, AstroSite } from "sst/constructs";
 
 export function API({ stack }: StackContext) {
   const api = new Api(stack, "api", {
+    defaults: {},
     routes: {
       "GET /api/trpc/{proxy+}": "packages/functions/src/trpc.handler",
       "POST /api/trpc/{proxy+}": "packages/functions/src/trpc.handler",
