14bd094b1fe23e4c3ab0e25638bb70a7faa97922
Taj K <taj.kirkpatrick@gmail.com>
2023-08-31 21:41:22 -0500

    feat: frontend saving to database with trpc

--- packages/web/src/lib/trpc.ts
+++ packages/web/src/lib/trpc.ts
@@ -1,6 +1,7 @@
-import type { AppRouter } from "../../../functions/src/trpc/router";
+import type { AppRouter } from "../../../core/src/trpc/config/router";
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 
+// @ts-ignore the AppRouter is imported from another parent folder and I believe causing the TS error
 export const trpcClient = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
